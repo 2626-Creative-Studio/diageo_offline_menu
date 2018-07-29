a138a3c94fc70116bf68aea1c3ed3ec0fd173bc4
Akshay Jain <jain.akshay5244@gmail.com>
2018-07-29 19:10:26 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module('offline-menu', []);
+
+app.controller('menuCtrl', function ($scope) {
+	console.log('menu is working...');
+	$scope.menu = [
+		{
+			name: "option1",
+			items_list: ["item1", "item2", "item3", "item4", "item5", "item6", "item7"]
+		},
+		{
+			name: "option2",
+			items_list: ["item1", "item2", "item3", "item4", "item5", "item6", "item7"]
+		},
+		{
+			name: "option3",
+			items_list: ["item1", "item2", "item3", "item4", "item5", "item6", "item7"]
+		},
+		{
+			name: "option4",
+			items_list: ["item1", "item2", "item3", "item4", "item5", "item6", "item7"]
+		},
+		{
+			name: "option5",
+			items_list: ["item1", "item2", "item3", "item4", "item5", "item6", "item7"]
+		}
+	];
+});

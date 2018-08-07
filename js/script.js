59cb4f963daa66a2e05389b0a8066d468799ec95
Akshay Jain <jain.akshay5244@gmail.com>
2018-08-07 12:53:55 +0530

    release

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
 
 app.controller('menuCtrl', function ($scope) {
 	console.log('menu is working...');
+
+	//$('#myModal4').modal();
+
+	$(document).keydown(function (event) {
+		if (event.keyCode == 27) {
+			console.log('Escape Key Pressed');
+			$('.modal').modal({ keyboard: true });
+			$('.modal').modal("hide");
+		}
+	});
+
+	$(".modal").on("shown.bs.modal", function () {
+		$(".modal").addClass("animated zoomIn");
+	});
+
+	$(".modal").on("hidden.bs.modal", function () {
+		//$(".modal").addClass("animated zoomOut");
+	});
+
 	$scope.menu = [
 		{
 			name: "option1",
